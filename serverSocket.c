5ddc8052f979831ae480b7adf2394e26570658b3
Vikhyath-Shetty <vikhyath.v.shetty@gmail.com>
2025-02-03 22:05:26 +0530

    binding completed

--- serverSocket.c
+++ serverSocket.c
@@ -1,21 +1,53 @@
+#define WIN32_LEAN_AND_MEAN
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
+
+void error(char *message)
+{
+  errno = WSAGetLastError();
+  perror(message);
+  WSACleanup();
+  exit(EXIT_FAILURE);
+}
 
 int main(int argc, char *argv[])
 {
   if (argc != 2)
   {
-    fprintf(stderr, "Usage:./socket.exe <port>");
+    fprintf(stderr, "\nUsage:./socket.exe <port>");
     exit(EXIT_FAILURE);
   }
 
-  // Initiate winsock dll process
+  // winsock initialization
   WSADATA wsa;
   if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
   {
-    printf("Failed to initialize winsock : %d", WSAGetLastError());
-    exit(EXIT_FAILURE);
+    error("\nWinsock failed to initialize");
   }
-  printf("Winsock initialized successfully");
+  printf("\nWinsock initialized successfully");
+
+  // Socket creation
+  SOCKET serverSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  
+  if (serverSock == INVALID_SOCKET)
+  {
+    error("\nSocket creation failed");
+  }
+  printf("\nSocket created successfully");
+
+  // Socket binding
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = inet_addr("127.0.0.1");
+  address.sin_port = htons(atoi(argv[1]));
+  if (bind(serverSock, (struct sockaddr *)&address, sizeof(address)) != 0)
+  {
+    error("\nBinding failed");
+  }
+  printf("\nSocket has been bound");
+
+  WSACleanup();
+  return 0;
 }
